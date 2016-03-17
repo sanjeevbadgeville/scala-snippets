1feaac04eab508cdd92cbc5961ba64a1a3d36c24
Sanjeev Chakravarty <sanjeev@badgeville.com>
2016-03-16 17:37:00 -0700

    Scala Shell Scripting in sending email using SendGrid example

---
+++ scala-scripts/email/SendGridScala.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+exec /opt/lang/scala/scala-2.11.7/bin/scala -classpath "lib/sendgrid-java.jar:." "$0" "$@"
+!#
+
+import com.sendgrid._
+
+object SendGridScala {
+  def main(args: Array[String]) {
+    println("Sending Email using SendGrid! " + args.toList)
+    val sendgrid:SendGrid = new SendGrid("<YOUR_API_KEY>")
+
+    val email:SendGrid.Email = new SendGrid.Email()
+    email.addTo("sanjeev@badgeville.com")
+    email.setFrom("other@example.com")
+    email.setSubject("Hello World")
+    email.setText("My first email with SendGrid Java!")
+
+    try {
+      val response: SendGrid.Response = sendgrid.send(email)
+      println(response.getMessage())
+    }
+    catch {
+      case e:SendGridException => println(e)
+    }
+  }
+}
+
+SendGridScala.main(args)
